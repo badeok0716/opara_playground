bf042c775e9fbb4c3b383f09ce837ab522d65144
badeok0716 <badeok0716@snu.ac.kr>
2024-12-04 17:12:38 +0900

    fix typo in env setup

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,4 @@
 conda create -n stream python=3.10 -y
 conda activate stream
-pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu117
+pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu118
 pip install -e .
